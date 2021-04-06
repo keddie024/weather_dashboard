520a3e352adaabfe816b285c775e6e4d37a784bc
Kevin Eddie <keddie024@gmail.com>
2021-04-05 21:38:54 -0400

    local storage working // buttons not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,50 @@ $(document).ready(function() {
     const myKey = "67312692d84f9dee845148cd2f7cd327";
     var searchButton = $("#search-button");
     var cityButton = $(".city-btn");
+    var oldCities = $("#searched-cities");
     var fiveDay = $("#5-day");
+    var cityList = [];
+    var cityParse = JSON.parse (localStorage.getItem("Cities"));
 
+    var init = function() {
 
-    // if (localStorage.length > 0) {
-    //     var cityList = JSON.parse (localStorage.getItem("Cities"));
-    // }
+        if (cityParse != null) {
 
-    // for (i = 1; i < cityList.length; i++) {
-    //     var 
-    // }
+            cityList = cityParse;
 
+            for (i = 0; i < cityList.length; i++) {
+                var oldCityBtn = document.createElement("button");
+                
+                oldCityBtn.textContent = cityList[i];
+                console.log(cityList[i]);
+                oldCityBtn.classList.add("btn");
+                oldCityBtn.classList.add("btn-primary");
+                oldCityBtn.classList.add("w-100");
+                oldCityBtn.classList.add("city-btn");
+                oldCityBtn.classList.add("mb-3");
+
+                oldCities.append(oldCityBtn);
+            }
+        
+        }
+
+    }
 
     var oldCity = function () {
+        console.log("clicked city");
         var city = $("#user-city");
         city.val(this.textContent);
         getWeather();
     }
 
     var getWeather = function() {
-        console.log("clicked");
         var city = $("#user-city").val();
-        console.log(city);
+        if (!city) {
+            location.reload();
+            return
+        }
+        cityList.push(city);
+        localStorage.setItem("Cities", JSON.stringify(cityList));
         var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + myKey + "&units=imperial";
 
         fetch(requestURL)
@@ -41,14 +63,30 @@ $(document).ready(function() {
 
             fiveDay.removeClass("hide");
 
+            requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + myKey + "&units=imperial";
 
-            // $("#city-UV").children().eq(4).text(data.name);
+            fetch(requestURL)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                $("#city-UV").text("UV Index : " + Math.floor(data.current.uvi));
+
+                for (i = 1; i < 6; i++) {
+                    var forecastCard = $("#card-" + i + "");
+                    forecastCard.children().eq(0).text(moment.unix(data.daily[i].dt).format("MM/DD/YY"));
+                    forecastCard.children().eq(1).text(Math.floor(data.daily[i].temp.day) + "°F");
+                    forecastCard.children().eq(2).text("Wind: " + Math.floor(data.daily[i].wind_speed) + " MPH");
+                    forecastCard.children().eq(3).text("Humidity: " + Math.floor(data.daily[i].humidity) + " %");
+                }
+            })
+            
         })
 
     }
 
     searchButton.on("click", getWeather);
-
     cityButton.on("click", oldCity);
+    init();
 
 })
