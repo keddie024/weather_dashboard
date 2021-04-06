6e85c30c33ebb3be67eb38c7c2cd335ec7c20145
Kevin Eddie <keddie024@gmail.com>
2021-04-05 22:29:33 -0400

    code generally complete and comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,30 @@
 $(document).ready(function() {
     const myKey = "67312692d84f9dee845148cd2f7cd327";
     var searchButton = $("#search-button");
-    var cityButton = $(".city-btn");
     var oldCities = $("#searched-cities");
     var fiveDay = $("#5-day");
     var cityList = [];
     var cityParse = JSON.parse (localStorage.getItem("Cities"));
 
+    // Starts when the page is loaded. Adds buttons to the left side of the page using cities from local storage.
     var init = function() {
 
         if (cityParse != null) {
 
             cityList = cityParse;
 
-            for (i = 0; i < cityList.length; i++) {
+            var uniqueList = [...new Set(cityList)];
+
+            for (i = 0; i < uniqueList.length; i++) {
                 var oldCityBtn = document.createElement("button");
                 
-                oldCityBtn.textContent = cityList[i];
-                console.log(cityList[i]);
+                oldCityBtn.textContent = uniqueList[i];
                 oldCityBtn.classList.add("btn");
                 oldCityBtn.classList.add("btn-primary");
                 oldCityBtn.classList.add("w-100");
-                oldCityBtn.classList.add("city-btn");
                 oldCityBtn.classList.add("mb-3");
+                oldCityBtn.classList.add("temp-button");
+                oldCityBtn.addEventListener("click", oldCity);
 
                 oldCities.append(oldCityBtn);
             }
@@ -31,13 +33,14 @@ $(document).ready(function() {
 
     }
 
+    // When a user clicks a previously searched city, this function calls the getWeather function for that city.
     var oldCity = function () {
-        console.log("clicked city");
         var city = $("#user-city");
         city.val(this.textContent);
         getWeather();
     }
 
+    // Fetches weather data from the API and then displays it on the page.
     var getWeather = function() {
         var city = $("#user-city").val();
         if (!city) {
@@ -53,16 +56,17 @@ $(document).ready(function() {
             return response.json();
         })
         .then(function(data) {
-            $("#city-name").text(data.name);
+            console.log(data);
             var locale = data.timezone / 60;
             var date = moment().utcOffset(locale).format("MM/DD/YY");
-            $("#current-date").text(date);
+            $("#city-name").text(data.name + " " + date);
             $("#city-temp").text("Temp: " + Math.floor(data.main.temp) + "°F");
             $("#city-wind").text("Wind: " + Math.floor(data.wind.speed) + " MPH");
             $("#city-hum").text("Humidity: " + Math.floor(data.main.humidity) + " %");
 
             fiveDay.removeClass("hide");
 
+            // Fetches data for the 5-day forecast and then displays it on the page.
             requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + myKey + "&units=imperial";
 
             fetch(requestURL)
@@ -70,7 +74,6 @@ $(document).ready(function() {
                 return response.json();
             })
             .then(function(data) {
-                $("#city-UV").text("UV Index : " + Math.floor(data.current.uvi));
 
                 for (i = 1; i < 6; i++) {
                     var forecastCard = $("#card-" + i + "");
@@ -86,7 +89,6 @@ $(document).ready(function() {
     }
 
     searchButton.on("click", getWeather);
-    cityButton.on("click", oldCity);
     init();
 
 })
